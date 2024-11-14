7bd5d3247399e9948159abe1e2797f4be38a5b4d
Patrick Hoffmann <p.hoffmann@berlin.de>
2024-11-15 00:32:42 +0100

    test new()

--- src/test.rs
+++ src/test.rs
@@ -1,4 +1,20 @@
 #[test]
+fn read_cur() {
+    use super::*;
+
+    let files = DirIterator::new()
+        .expect("path not found")
+        .flatten()
+        .map(|e| e.file_name().as_os_str().to_string_lossy().to_string())
+        .collect::<Vec<_>>();
+
+    assert!(files.contains(&"test.rs".to_string()));
+    assert!(files.contains(&"lib.rs".to_string()));
+    assert!(files.contains(&"Cargo.toml".to_string()));
+    assert!(files.contains(&"README.md".to_string()));
+}
+
+#[test]
 fn read_dir() {
     use super::*;
 
